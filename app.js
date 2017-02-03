4e3fd2f2b5ae3c67ed906600e3251ab4c1fe0d96
Juspreet95 <juspreet.singh1@gmail.com>
2017-02-03 17:45:42 +0100

    start tp3

--- app.js
+++ app.js
@@ -12,6 +12,55 @@ app.set( 'view engine', 'ejs' );
 //setting the 'assets' directory as our static assets dir (css, js, img, etc...)
 app.use( '/assets', express.static( 'assets' ) );
 
+function callLeboncoin() {
+
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1084457186.htm?ca=12_s'
+
+
+
+    request( url, function ( error, response, html ) {
+
+        if ( !error && response.statusCode == 200 ) {
+
+
+
+            var $ = cheerio.load( html )
+
+
+
+            var lbcDataArray = $( 'section.properties span.value' )
+
+
+
+            var lbcData = {
+
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
+
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+
+            }
+
+
+
+            console.log( lbcData )
+
+
+
+        }
+
+        else {
+
+            console.log( error )
+
+        }
+
+    })
+
+}
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
